c7377d5af4bf459b763e0fff5bcf4cbe8911cdad
Soumya-RanjanBhoi <soumya6ranjan9@gmail.com>
2025-10-09 19:21:36 +0530

    adding& updating files for  data validation

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.logging import logger
+from textSummarizer.pipeline.stage_2_data_validation import DataValidationTrainingPipeline
 
-STAGE_NAME="Data Ingestion stage"
+STAGE_NAME="Data validation stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    data_ingestion=DataIngestionTrainingPipeline()
-    data_ingestion.main()
+    data_validation=DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<< \n\n x=====x")
 
 except Exception as e:
---
+++ src/textSummarizer/pipeline/stage_2_data_validation.py
@@ -0,0 +1,16 @@
+from textSummarizer.config.configuration import ConfriguationManager
+from textSummarizer.components.data_validation import DataValidation
+from textSummarizer.logging import logger
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        try:
+           config=ConfriguationManager()
+           data_validation_config=config.get_data_validation_config()
+           data_validation=DataValidation(config=data_validation_config)
+           data_validation.validate_all_files_exists()
+        except Exception as e:
+          raise e
