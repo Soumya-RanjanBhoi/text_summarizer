fa2434d7c48feb55e760f44145d59a7956223726
Soumya-RanjanBhoi <soumya6ranjan9@gmail.com>
2025-10-12 11:51:50 +0530

    updated main.py

--- main.py
+++ main.py
@@ -4,39 +4,39 @@ from textSummarizer.pipeline.stage_2_data_validation import DataValidationTraini
 from textSummarizer.pipeline.stage3_data_transformation import DataTransformationTrainingPipeline
 from textSummarizer.pipeline.stage4_model_evaluation import ModelEvaluationTrainingPipeline
 
-# STAGE_NAME="Data Ingestion stage"
-# try:
-#     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#     data_ingestion=DataIngestionTrainingPipeline()
-#     data_ingestion.main()
-#     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<< \n\n x=====x")
+STAGE_NAME="Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion=DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<< \n\n x=====x")
 
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+except Exception as e:
+    logger.exception(e)
+    raise e
 
-# STAGE_NAME="Data validation stage"
-# try:
-#     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#     data_validation=DataValidationTrainingPipeline()
-#     data_validation.main()
-#     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<< \n\n x=====x")
+STAGE_NAME="Data validation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_validation=DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<< \n\n x=====x")
 
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+except Exception as e:
+    logger.exception(e)
+    raise e
 
 
-# STAGE_NAME="Data transformation stage"
-# try:
-#     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#     data_transformation=DataTransformationTrainingPipeline()
-#     data_transformation.main()
-#     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<< \n\n x=====x")
+STAGE_NAME="Data transformation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_transformation=DataTransformationTrainingPipeline()
+    data_transformation.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<< \n\n x=====x")
 
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+except Exception as e:
+    logger.exception(e)
+    raise e
 
 
 STAGE_NAME="model evaluation stage"
