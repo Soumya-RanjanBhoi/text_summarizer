70fa496ec113e530f403f4c5ea54291601e6129f
Soumya-RanjanBhoi <soumya6ranjan9@gmail.com>
2025-10-12 11:51:15 +0530

    updated evaluation files

--- src/textSummarizer/pipeline/stage4_model_evaluation.py
+++ src/textSummarizer/pipeline/stage4_model_evaluation.py
@@ -12,6 +12,6 @@ class ModelEvaluationTrainingPipeline:
             config=ConfriguationManager()
             model_evaluation_config=config.get_model_evaluation_config()
             model_evaluation=ModelEvaluation(config=model_evaluation_config)
-            model_evaluation.convert()
+            model_evaluation.evaluate()
         except Exception as e:
             raise e
