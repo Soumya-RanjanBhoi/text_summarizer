8b1c95ed90571b3b3caecd81d308e9bec2c16594
Soumya Ranjan Bhoi <soumya6ranjan9@gmail.com>
2025-10-15 01:52:47 +0530

    Update prediction.py

--- src/textSummarizer/pipeline/prediction.py
+++ src/textSummarizer/pipeline/prediction.py
@@ -1,41 +1,41 @@
-from textSummarizer.config.configuration import ConfriguationManager
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
+from textSummarizer.config.configuration import ConfriguationManager
 
 class PredictionPipelines:
     def __init__(self):
         self.config = ConfriguationManager()
 
-    def summarize(self, text, text_len):
         eval_config = self.config.get_model_evaluation_config()
-        tokenizer = AutoTokenizer.from_pretrained(eval_config.model_path)
-        model = AutoModelForSeq2SeqLM.from_pretrained(eval_config.model_path)
+        translation_config = self.config.get_model_translation_config()
 
-        if text_len == 80:
-            len_penalty = 2
-        elif text_len == 180:
-            len_penalty = 1.0
-        elif text_len > 300:
-            len_penalty = 0.8
-        else:
-            len_penalty = 1.2
+        self.summarizer_tokenizer = AutoTokenizer.from_pretrained(eval_config.model_path)
+        self.summarizer_model = AutoModelForSeq2SeqLM.from_pretrained(eval_config.model_path)
+
+        self.translation_tokenizer = AutoTokenizer.from_pretrained(translation_config.tokenizer_path)
+        self.translation_model = AutoModelForSeq2SeqLM.from_pretrained(translation_config.model_path)
+
+        self.summarizer_pipe = pipeline(
+            "summarization", model=self.summarizer_model, tokenizer=self.summarizer_tokenizer
+        )
+        self.trans_pipe = pipeline(
+            "translation", model=self.translation_model, tokenizer=self.translation_tokenizer,
+            max_length=1024, truncation=True
+        )
+
+    def summarize(self, text, text_len):
 
         gen_kwargs = {
-            "num_beams": 4,
-            "max_length": text_len,
-            "min_length": 25,
-            "length_penalty": len_penalty,
-            "early_stopping": True
-        }
+        "num_beams": 4,
+        "max_length": text_len,
+        "min_length": int(text_len * 0.6),
+        "length_penalty": 0.9,
+        "early_stopping": True,
+        "truncation": False
+    }
 
-        summarizer_pipe = pipeline("summarization", model=model, tokenizer=tokenizer)
-        output = summarizer_pipe(text, **gen_kwargs)[0]['summary_text']
+        output = self.summarizer_pipe(text, **gen_kwargs)[0]['summary_text']
         return output
 
     def translate(self, text):
-        translation_config = self.config.get_model_translation_config()
-        tokenizer = AutoTokenizer.from_pretrained(translation_config.tokenizer_path)
-        model = AutoModelForSeq2SeqLM.from_pretrained(translation_config.model_path)
-
-        trans_pipe = pipeline("translation", model=model, tokenizer=tokenizer, max_length=1024, truncation=True)
-        output = trans_pipe(text)[0]['translation_text']
+        output = self.trans_pipe(text)[0]['translation_text']
         return output
